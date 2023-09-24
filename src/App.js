17eb5351e0b58069e23d8953d5654d3eeea557a5
Edwin Chen <edwinchenyj@gmail.com>
2023-09-24 20:52:46 +0000

    Canvas with React app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CanvasComponent from './CanvasComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const canvasRef = React.useRef(null);
+
+    const handlePredict = () => {
+        // Prediction code will go here.
+        alert('Predicting...');
+    }
+
+    const handleClear = () => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    return (
+        <div>
+            <CanvasComponent ref={canvasRef} width={280} height={280} />
+            <button onClick={handlePredict}>Predict</button>
+            <button onClick={handleClear}>Clear</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/CanvasComponent.js
@@ -0,0 +1,48 @@
+import React, { useRef, useEffect, useState } from 'react';
+
+const CanvasComponent = (props) => {
+    const canvasRef = useRef(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        canvas.width = props.width;
+        canvas.height = props.height;
+    }, [props.width, props.height]);
+
+    const startDrawing = (e) => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        setIsDrawing(true);
+    };
+
+    const draw = (e) => {
+        if (!isDrawing) return;
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        ctx.lineWidth = 5;
+        ctx.lineCap = "round";
+        ctx.strokeStyle = "black";
+
+        ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    };
+
+    const stopDrawing = () => {
+        setIsDrawing(false);
+    };
+
+    return <canvas
+        ref={canvasRef}
+        onMouseDown={startDrawing}
+        onMouseUp={stopDrawing}
+        onMouseOut={stopDrawing}
+        onMouseMove={draw}
+    />;
+}
+
+export default CanvasComponent;
